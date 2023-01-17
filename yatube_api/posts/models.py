0df54cb44ec202028d06d4fbceba0788ca4cac8a
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-01-17 17:22:35 +0300

    Comment

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -54,12 +54,21 @@ class Post(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+        related_name='comments'
+    )
     text = models.TextField()
     created = models.DateTimeField(
-        'Дата добавления', auto_now_add=True, db_index=True)
+        'Дата добавления',
+        auto_now_add=True,
+        db_index=True
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE, 
+        related_name='comments'
+    )
 
 # в проекте должна быть описана модель Follow,
 # в ней должно быть два поля —
