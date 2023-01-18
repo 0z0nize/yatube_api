0d7f0fc40d2e0c2c81e6fda17a0cf3cc8d6b2108
Владислав Шкаровский <vladislav.shkarovskiy@yandex.ru>
2023-01-19 02:28:24 +0300

    API для Yatube v02

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -24,7 +24,7 @@ class CommentViewSet(IsAuthOrReadOnlyViewSet):
 
     def get_queryset(self):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
-        return post.comments.select_related('author').all()
+        return post.comments
 
     def perform_create(self, serializer):
         post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
